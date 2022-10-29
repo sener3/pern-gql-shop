7fecb191e1f1c14080bcc9ed3ca84ec878b63944
sener21 <ali.sener@webmarc.io>
2022-10-30 02:09:49 +0300

    feat: add new helper functions

--- client/src/utils/helpers.ts
+++ client/src/utils/helpers.ts
@@ -1,3 +1,7 @@
 export const cls = (...args: string[]): string => {
     return args.filter(Boolean).join(" ");
 };
+
+export const capitalize = (word: string): string => {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}
