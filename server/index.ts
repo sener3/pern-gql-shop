3b6cebfb87203c91dcf558be83fecb3919d00004
sener21 <ali.sener@webmarc.io>
2022-10-30 02:21:14 +0300

    feat: apollo server config

--- server/index.ts
+++ server/index.ts
@@ -1,52 +1,36 @@
-import 'dotenv/config';
-import { createServer } from 'http';
+import "dotenv/config";
+import cors from "cors";
+import path from "path";
 import express from "express";
 
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import { WebSocketServer } from 'ws';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import { useServer } from 'graphql-ws/lib/use/ws';
+import { ApolloServer } from "apollo-server-express";
+import { graphqlUploadExpress } from "graphql-upload-ts";
 
-import { typeDefs } from './graphql/typeDefs';
-import { resolvers } from './graphql/resolvers';
+import { typeDefs } from "./graphql/typeDefs";
+import { resolvers } from "./graphql/resolvers";
 
 async function startApolloServer(typeDefs: any, resolvers: any) {
-  const app = express();
-  
-  const httpServer = createServer(app);
-  
-  const schema = makeExecutableSchema({ typeDefs, resolvers });
-  
-  const wsServer = new WebSocketServer({
-    server: httpServer,
-    path: '/subscriptions',
-  });
-  
-  const serverCleanup = useServer({ schema }, wsServer);
-  
   const server = new ApolloServer({
-    schema,
-    plugins: [
-      ApolloServerPluginDrainHttpServer({ httpServer }),
-      {
-        async serverWillStart() {
-          return {
-            async drainServer() {
-              await serverCleanup.dispose();
-            },
-          };
-        },
-      },
-    ],
+    typeDefs,
+    resolvers,
   });
   
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-  });
+  const app = express();
+  app.use(cors());
+  app.use("/images", express.static(path.join(__dirname, "images")))
+  app.use(graphqlUploadExpress({ maxFileSize: 10 * 1024 * 1024, maxFiles: 10 }));
 
-  console.log(`🚀  Server ready at: ${url}`);
+  await server.start();
+  
+  server.applyMiddleware({
+    app,
+    path: "/",
+    cors: false,
+  })
+
+  app.listen({ port: 4000 })
+
+  console.log(`🚀  Server ready at: http://localhost:4000`);
 }
 
 startApolloServer(typeDefs, resolvers);
