fcf729383cf5187614ed96f103fee0132a1a6a7c
sener21 <ali.sener@webmarc.io>
2022-10-30 02:05:41 +0300

    feat: Box component

--- client/src/components/ui/Box/Box.tsx
+++ client/src/components/ui/Box/Box.tsx
@@ -1,19 +1,23 @@
+import { forwardRef } from "react";
 import { cls } from "@utils/helpers";
 
 type BoxProps = {
     id?: string;
     className?: string;
     children?: React.ReactNode | React.ReactNode[];
+
+    onClick?: () => void;
 };
 
-const Box = (props: BoxProps): JSX.Element => {
-    const { id = "", className = "", children } = props;
-
-    return (
-        <div id={id} className={cls(className)}>
-            {children}
-        </div>
-    );
-};
+const Box = forwardRef<HTMLDivElement, BoxProps>((props, ref) => (
+    <div
+        ref={ref}
+        id={props.id}
+        className={cls(props.className ?? "")}
+        onClick={props.onClick}
+    >
+        {props.children}
+    </div>
+));
 
 export default Box;
