e9d9ac8fcb0f4677899ecf0a920b70f4679e3ee3
sener21 <ali.sener@webmarc.io>
2022-10-30 02:06:10 +0300

    feat: Hyperlink component

--- client/src/components/ui/Hyperlink/Hyperlink.tsx
+++ client/src/components/ui/Hyperlink/Hyperlink.tsx
@@ -1,16 +1,25 @@
 import { Link } from "react-router-dom";
 
+import "@styles/components/hyperlink.scss";
+
+import { cls } from "@utils/helpers";
+
+type HyperlinkVariant = "primary" | "secondary";
+
 type HyperlinkProps = {
-    className?: string;
     to?: string;
+    variant?: HyperlinkVariant;
+    className?: string;
     children: React.ReactNode | React.ReactNode[];
 };
 
 function Hyperlink(props: HyperlinkProps): JSX.Element {
-    const { className, to = "/", children } = props;
+    const { className = "", to = "/", variant = "", children } = props;
+
+    const classes = cls(className, (variant && `link-${variant}`) || "");
 
     return (
-        <Link className={className} to={to}>
+        <Link className={classes} to={to}>
             {children}
         </Link>
     );
