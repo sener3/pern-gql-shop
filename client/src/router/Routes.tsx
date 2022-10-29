6bebc7decb4dd0a60cdb7236c51078f77df60035
sener21 <ali.sener@webmarc.io>
2022-10-30 02:08:39 +0300

    feat: routing config

--- client/src/router/Routes.tsx
+++ client/src/router/Routes.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@pages/Home";
 import Search from "@pages/Search";
 import Category from "@pages/Category";
+import Product from "@pages/Product";
+
+import AdminProduct from "@pages/admin/Product";
 
 const MyRoutes = (): JSX.Element => {
     return (
@@ -11,6 +14,9 @@ const MyRoutes = (): JSX.Element => {
                 <Route path="/" element={<Home />} />
                 <Route path="/search" element={<Search />} />
                 <Route path="/category/:id" element={<Category />} />
+                <Route path="/product/:id" element={<Product />} />
+
+                <Route path="/admin/product" element={<AdminProduct />} />
             </Routes>
         </BrowserRouter>
     );
